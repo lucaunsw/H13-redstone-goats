2ba43bd346d5106d3b7cf4eec6caa7b2e14caf27
Zachary Abran <z5589903@ad.unsw.edu.au>
2025-03-09 14:43:06 +1100

    [ZA][FIX] Added config.json for PORT and URL for server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import config from "../config.json";
 import cors from "cors";
 import morgan from "morgan";
 
@@ -13,7 +14,7 @@ app.use(cors());
 // Middleware for logging HTTP requests
 app.use(morgan("dev"));
 
-const PORT = parseInt(process.env.PORT || "3000", 10);
+const PORT = parseInt(process.env.PORT || config.port);
 const HOST = process.env.IP || "127.0.0.1";
 
 // ===========================================================================
