5395225d9c4a3ee33c4dd76aa6cc779203764be7
lucaunsw <lucajn101@gmail.com>
2025-03-10 16:25:01 +1100

    [LN][FEAT] completed orderCancel server, implementation and test skeleton

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+const orderCancel = (userId: string, orderId: string, reason: string) => {
+  //   const data = getData();
+  //   if (!data.orders || !data.orders[userId]) {
+  //     throw new Error("invalid userId");
+  //   }
+  //   const userOrders = data.orders[userId];
+  //   const order = userOrders.find((o) => o.orderId === orderId);
+  //   if (!order) {
+  //     throw new Error("invalid orderId");
+  //   }
+  //   if (order.status === "cancelled") {
+  //     throw new Error("order already cancelled");
+  //   }
+  //   order.status = "cancelled";
+  //   saveData(data);
+  //   return { reason };
+};
+
+export { orderCancel };
