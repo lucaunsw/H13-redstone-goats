7ec3f5a3cf236739dc467ad657525c5daf26baba
Joshua Mamelok <j.mamelok@student.unsw.edu.au>
2025-03-17 09:47:24 +1100

    [JM][FIX] Fixed DB clear functions not waiting to finish

--- src/tests/orderCreate.test.ts
+++ src/tests/orderCreate.test.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { userRegister } from './testHelper';
-import { SessionId, Order, User, 
-  Item, BillingDetails } from '../types';
-=======
 import { userRegister, reqHelper } from './testHelper';
-import { SessionId, OrderParam, UserParam, 
-  Item, BillingDetailsParam, DeliveryInstructions } from '../types';
->>>>>>> fcedd1fa2f4ac1395e422dea1ebddfbaa26fab4f
+import { SessionId, Order, User, 
+  Item, BillingDetails, DeliveryInstructions } from '../types';
 import { getPostResponse } from '../wrapper';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
@@ -33,10 +27,10 @@ function requestOrderCreate(
 // let user: {body: { token: SessionId }};
 let userId: number;
 let testName: string;
-let testUser: UserParam;
-let testSeller: UserParam;
+let testUser: User;
+let testSeller: User;
 let testItem: Item;
-let testBillingDetails: BillingDetailsParam;
+let testBillingDetails: BillingDetails;
 let testDeliveryDetails: DeliveryInstructions;
 const date = new Date().toISOString().split('T')[0];
 
@@ -82,7 +76,7 @@ beforeEach(async () => {
   };
   testDeliveryDetails = {
     streetName: 'White St',
-    citName: 'Sydney',
+    cityName: 'Sydney',
     postalZone: '2000',
     countrySubentity: 'NSW',
     addressLine: '33 White St, Sydney NSW',
@@ -121,49 +115,7 @@ describe.skip('Test orderCreate route', () => {
     expect(response.statusCode).toBe(401);
   });
 
-<<<<<<< HEAD
-// let user: {body: { token: SessionId }};
-// let userId: number;
-// let name: string;
-// let user: User;
-// let testItem: Item;
-// let testBillingDetails: BillingDetails;
-// const date = new Date().toISOString().split('T')[0];;
-
-// beforeEach(() => {
-//   // clear function
-//   name = 'Bobby Jones'
-//   userId = userRegister(
-// 	  'BobbyJones@gmail.com',
-// 	  'cake',
-// 	  'Bobby',
-// 	  'Jones}');
-
-//   user = {
-//     userId: userId,
-//     name: name,
-//   };
-//   testItem = {
-//     id: 123,
-//     name: 'soap',
-//     price: 5,
-//     description: 'This is soap',
-//   };
-//   testBillingDetails = {
-//     creditCardNumber: 1000000000000000,
-//     CVV: 111,
-//     expiryDate: date,
-//   };
-// });
-
-
-// describe.skip('Test orderCreate route', () => {
-
-//   test('Error from invalid token', () => {
-//     const invalidUserId = userId + 1; 
-=======
   test('Error from invalid name', () => {
->>>>>>> fcedd1fa2f4ac1395e422dea1ebddfbaa26fab4f
     
     const body = {
       items: [testItem],
