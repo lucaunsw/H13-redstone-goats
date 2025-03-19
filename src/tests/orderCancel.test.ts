ad3f7b2e09e7a184a8d3fc0ded1814e21bd89f40
lucaunsw <lucajn101@gmail.com>
2025-03-20 01:43:14 +1100

    [LN][FIX] fixed failing orderConfirm and orderCancel tests

--- src/tests/orderCancel.test.ts
+++ src/tests/orderCancel.test.ts
@@ -3,26 +3,12 @@ const SERVER_URL = `http://127.0.0.1:3200`;
 const TIMEOUT_MS = 20 * 1000;
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { BillingDetails, DeliveryInstructions, Item, Order, UserSimple } from "../types";
-import { reqHelper, userRegister} from "./testHelper";
-import { getLatestOrderXML, getOrder } from "../dataStore";
+import { BillingDetails, DeliveryInstructions, Item, UserSimple } from "../types";
+import { reqHelper, userRegister, requestOrderCreate} from "./testHelper";
 dotenv.config();
 
 function getPutResponse(route: string, body: { [key: string]: unknown }) {
   const res = request("PUT", SERVER_URL + route, {
-    json: body,
-    timeout: TIMEOUT_MS,
-  });
-  return {
-    body: JSON.parse(res.body.toString()),
-    statusCode: res.statusCode,
-  };
-}
-
-async function requestOrderCreate(
-  body: Order,
-) {
-  const res = request("POST", SERVER_URL + `/v1/order/create`, {
     json: body,
     timeout: TIMEOUT_MS,
   });
@@ -59,65 +45,65 @@ let testDeliveryDetails: DeliveryInstructions;
 const date = new Date().toISOString().split('T')[0];
 
 beforeEach(async () => {
-  await reqHelper('DELETE', '/v1/clear');
-  testName = 'Bobby Jones'
-
-  const token = await userRegister(
-    'example10@email.com', 
-    'example123', 
-    'Bobby', 
-    'Jones').body.token;
-  const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: number };
-  userId = decoded.userId;
-
-  const sellerToken = await userRegister(
-    'example20@email.com', 
-    'example123', 
-    'Test', 
-    'Seller').body.token;
-  const sellerId = (jwt.verify(token, process.env.JWT_SECRET as string) as { userId: number }).userId;
-
-  testSeller = {
-    id: sellerId,
-    name: 'Test Seller',
-    streetName: 'Yellow St',
-    cityName: 'Brisbane',
-    postalZone: '4000',
-    cbcCode: 'AU'
-  };
-  testItem = {
-    id: 123,
-    name: 'soap',
-    seller: testSeller,
-    price: 5,
-    description: 'This is soap',
-  };
-  testBuyer = {
-    id: userId,
-    name: testName,
-    streetName: 'White St',
-    cityName: 'Sydney',
-    postalZone: '2000',
-    cbcCode: 'AU',
-  };
-  testBillingDetails = {
-    creditCardNumber: 1000000000000000,
-    CVV: 111,
-    expiryDate: date,
-  };
-  testDeliveryDetails = {
-    streetName: 'White St',
-    cityName: 'Sydney',
-    postalZone: '2000',
-    countrySubentity: 'NSW',
-    addressLine: '33 White St, Sydney NSW',
-    cbcCode: 'AU',
-    startDate: new Date(2025, 9, 5).toISOString().split('T')[0],
-    startTime: '13:00',
-    endDate: new Date(2025, 9, 10).toISOString().split('T')[0],
-    endTime: '13:00'
-  }
-});
+    await reqHelper('DELETE', '/v1/clear');
+    testName = 'Bobby Jones'
+  
+    const token = await userRegister(
+      'example10@email.com', 
+      'example123', 
+      'Bobby', 
+      'Jones').body.token;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: number };
+    userId = decoded.userId;
+  
+    const sellerToken = await userRegister(
+      'example20@email.com', 
+      'example123', 
+      'Test', 
+      'Seller').body.token;
+    const sellerId = (jwt.verify(sellerToken, process.env.JWT_SECRET as string) as { userId: number }).userId;
+  
+    testSeller = {
+      id: sellerId,
+      name: 'Test Seller',
+      streetName: 'Yellow St',
+      cityName: 'Brisbane',
+      postalZone: '4000',
+      cbcCode: 'AU'
+    };
+    testItem = {
+      id: 123,
+      name: 'soap',
+      seller: testSeller,
+      price: 5,
+      description: 'This is soap',
+    };
+    testBuyer = {
+      id: userId,
+      name: testName,
+      streetName: 'White St',
+      cityName: 'Sydney',
+      postalZone: '2000',
+      cbcCode: 'AU',
+    };
+    testBillingDetails = {
+      creditCardNumber: 1000000000000000,
+      CVV: 111,
+      expiryDate: date,
+    };
+    testDeliveryDetails = {
+      streetName: 'White St',
+      cityName: 'Sydney',
+      postalZone: '2000',
+      countrySubentity: 'NSW',
+      addressLine: '33 White St, Sydney NSW',
+      cbcCode: 'AU',
+      startDate: new Date(2025, 9, 5).toISOString().split('T')[0],
+      startTime: '13:00',
+      endDate: new Date(2025, 9, 10).toISOString().split('T')[0],
+      endTime: '13:00'
+    }
+  });
 
 describe("orderCancel successful return", () => {
   test("Should cancel an order successfully", async () => {
